da3d0c16e82b9a8ac31697a9cd19e99712c66fff
Ogulbeg <aolya0722@gmail.com>
2020-04-16 11:31:30 -0400

    cucumber tags hooks

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,20 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    glue = "com/vytrack/step_definitions" ,
+    features = "src/test/resources/features",
+        //dryRun=true  checking if every test has code implementation, otherwise cucumber wil create it
+        dryRun = false, // execute test
+        strict = false,
+        tags = "", // for skipping this method "@driver" for running only this method
+//it will run one which execute but if we have both it will run both
+        plugin = "html:target/cucumber-html-report"
+)
+public class CucumberRunner {
+    
+}
